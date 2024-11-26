072a6745b0546e792bbf1a7b1f73a61e431880dd
Osama Hendawi <osama.hendawi@hotmail.com>
2024-11-26 11:41:22 +0000

    feat: set up views & models for infinte scroll on homepage

--- post/views.py
+++ post/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.views import generic
+from django.core.paginator import Paginator
 from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
     queryset = Post.objects.all()
     template_name = "post/index.html"
+
+
+def homepage(request):
+    page_number = request.GET.get('page', 1)
+    queryset = Paginator(Post.objects.all(), 3)
+    page_obj = queryset.get_page(page_number)
+
+    return render(request, 
+            "post/index.html", 
+            {'page_obj' : page_obj},
+            )
