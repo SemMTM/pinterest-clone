bf7ae84d41865a575760e25cec8e5a22251e5e9f
Osama Hendawi <osama.hendawi@hotmail.com>
2024-11-27 10:11:52 +0000

    feat: add infinite scroll with HTMX

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 - How to pin navbar to the bottom of the screen: https://forum.builder.io/t/how-do-you-pin-a-nav-bar-to-the-bottom-of-the-screen/32
 - CSS Masonry layout: https://kulor.medium.com/pinterest-style-masonry-layout-using-pure-css-493c1206d01d
 - CSS image overlay: https://www.w3schools.com/howto/howto_css_image_overlay_title.asp
+- Infinite scroll in Django using HTMX: https://www.fmacedo.com/posts/1-django-htmx-infinite-scroll
--- post/views.py
+++ post/views.py
@@ -7,11 +7,19 @@ from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.all()
     template_name = "post/index.html"
+    paginate_by = 3
+    context_object_name = "userimages"
+
+    def get_template_names(self, *args, **kwargs):
+        if self.request.htmx:
+            return "post/image-list.html"
+        else:
+            return self.template_name
 
 
 def homepage(request):
     page_number = request.GET.get('page', 1)
-    queryset = Paginator(Post.objects.all(), 3)
+    queryset = Paginator(Post.objects.all(), 4)
     page_obj = queryset.get_page(page_number)
 
     return render(request, 
