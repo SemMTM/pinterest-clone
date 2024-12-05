8cf84d588792f366e6be80222cd74c6cd9f89f4e
Osama Hendawi <osama.hendawi@hotmail.com>
2024-12-05 14:48:47 +0000

    feat: add comment input area w placeholder

--- post/forms.py
+++ post/forms.py
@@ -3,6 +3,13 @@ from django import forms
 
 
 class CommentForm(forms.ModelForm):
+    body = forms.CharField(
+        widget=forms.Textarea(
+            attrs={"placeholder": "Add a comment",
+                    'rows': 2,}
+        ),
+    )
+
     class Meta:
         model = Comment
         fields = ('body',)
--- post/models.py
+++ post/models.py
@@ -31,7 +31,7 @@ class Comment(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="commenter"
     )
-    body = models.TextField()
+    body = models.CharField(max_length=280)
     created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
