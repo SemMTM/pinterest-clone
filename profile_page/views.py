5e5732e29bcea81179f006fecf4a4eea7a600b0a
Osama Hendawi <osama.hendawi@hotmail.com>
2024-12-11 11:32:48 +0000

    feat: user profile page when username clicked on post_detail page

--- profile_page/views.py
+++ profile_page/views.py
@@ -3,8 +3,12 @@ from .models import Profile
 
 # Create your views here.
 
-def profile_page(request):
+def profile_page(request, user):
+    queryset = Profile.objects.all()
+    profile = get_object_or_404(queryset, user=user)
+
     return render(
         request,
         "profile_page/profile_page.html",
+        {"profile": profile},
     )
