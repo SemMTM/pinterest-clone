36399e3369ab4a6de04d41dde45d3e344848c11d
Osama Hendawi <osama.hendawi@hotmail.com>
2024-12-11 15:44:56 +0000

    feat: when profile button pressed, a profile is created for user if an instance does not exist

--- profile_page/views.py
+++ profile_page/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from django.http import Http404, HttpResponse
 from .models import Profile
 
 # Create your views here.
 
 def profile_page(request, user):
+    # Checks if user has a profile, if they do not then one is created for them.
+    try:
+        queryset = Profile.objects.all()
+        profile = get_object_or_404(queryset, user=user)
+    except Http404:
+        user = request.user
+        profile = Profile(
+            user=user
+        )
+        profile.save()
+
     queryset = Profile.objects.all()
-    profile = get_object_or_404(queryset, user=user)
 
     return render(
         request,
