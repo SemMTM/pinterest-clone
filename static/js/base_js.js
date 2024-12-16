e12422ff6bf02c94db06023798efd97d598f638d
Osama Hendawi <osama.hendawi@hotmail.com>
2024-12-16 10:41:56 +0000

    feat: add a back button

---
+++ static/js/base_js.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const backButton = document.getElementById('back-button');
+
+    if (backButton) {
+        backButton.addEventListener('click', function (e) {
+            if (document.referrer) {
+                // Prevent default link behavior and navigate to referrer
+                e.preventDefault();
+                window.location.href = document.referrer;
+            } else {
+                // Allow the default link behavior (fallback to href)
+            }
+        });
+    }
+});
