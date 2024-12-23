59a1fbf0d911a17f64ef4126c7fa9d6bb371045a
Osama Hendawi <osama.hendawi@hotmail.com>
2024-12-23 12:19:11 +0000

    feat: toggle between created and saved with no issues

--- static/js/profile_page_js.js
+++ static/js/profile_page_js.js
@@ -1,33 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Load created posts script
     const createdButton = document.getElementById('created-btn');
     const createdContainer = document.getElementById('created-container');
     const savedContainer = document.getElementById('saved-container');
-    const username = createdContainer.dataset.username; // Fetch the username from the data attribute
-
-    //createdButton.addEventListener('click', async () => {
-        //createdButton.disabled = true;
-        //try {
-        //    const fetchUrl = `/profile/${username}/created/`; // Construct the URL using the username
-        //    const response = await fetch(fetchUrl);
-        //    if (!response.ok) {
-        //        throw new Error(`Failed to load created pins. Status: ${response.status}`);
-        //    }
-        //    const html = await response.text();
-        //    createdContainer.innerHTML = html; // Inject the fetched HTML into the container;
-        //} catch (error) {
-        //    console.error('Error loading created posts:', error);
-        //    alert('Error loading created posts. Please try again later.');
-        //} finally {
-        //    createdButton.disabled = false; 
-        //}
-    //});
-
     const savedButton = document.getElementById('saved-btn');
 
     const toggleActive = (buttonToActivate, buttonToDeactivate) => {
         buttonToActivate.classList.add('active');
         buttonToDeactivate.classList.remove('active');
+
+        if (buttonToDeactivate != createdButton) {
+            createdButton.style.pointerEvents = 'none';
+            createdButton.style.cursor = 'not-allowed';
+        } else {
+            createdButton.style.pointerEvents = '';
+            createdButton.style.cursor = ''; // Reactivate the saved button when it's not active
+        }
     };
 
     const toggleHidden = (addHidden, removeHidden) => {
@@ -38,11 +25,12 @@ document.addEventListener('DOMContentLoaded', () => {
     createdButton.addEventListener('click', () => {
         toggleActive(createdButton, savedButton);
         toggleHidden(savedContainer, createdContainer);
+        createdContainer.classList.add('image-grid');
     });
     savedButton.addEventListener('click', () => {
         toggleActive(savedButton, createdButton);
         toggleHidden(createdContainer, savedContainer);
-
+        createdContainer.classList.remove('image-grid');
     });
 
     const editBoardButton = document.getElementById('edit-board-btn');
