d05e0b1fca95840b61a28a45e0318d9fcf3f7c7e
Osama Hendawi <osama.hendawi@hotmail.com>
2024-12-23 10:07:12 +0000

    feat: toggle masonry grid when swicthing between grids on profile page

--- static/js/profile_page_js.js
+++ static/js/profile_page_js.js
@@ -7,8 +7,6 @@ document.addEventListener('DOMContentLoaded', () => {
     createdButton.addEventListener('click', async () => {
         try {
             const fetchUrl = `/profile/${username}/created/`; // Construct the URL using the username
-            console.log(`Fetching from URL: ${fetchUrl}`);
-
             const response = await fetch(fetchUrl);
             if (!response.ok) {
                 console.error(`HTTP Error: ${response.status} ${response.statusText}`);
@@ -17,13 +15,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
             const html = await response.text();
             createdContainer.innerHTML = html; // Inject the fetched HTML into the container
-            console.log('Created posts loaded successfully.');
         } catch (error) {
             console.error('Error loading created posts:', error);
             alert('Error loading created posts. Please try again later.');
         }
     });
 
+
+    // Boards toggles scripts
     const savedButton = document.getElementById('saved-btn');
 
     const toggleActive = (buttonToActivate, buttonToDeactivate) => {
@@ -31,8 +30,15 @@ document.addEventListener('DOMContentLoaded', () => {
         buttonToDeactivate.classList.remove('active');
     };
 
-    createdButton.addEventListener('click', () => toggleActive(createdButton, savedButton));
-    savedButton.addEventListener('click', () => toggleActive(savedButton, createdButton));
+    createdButton.addEventListener('click', () => {
+        toggleActive(createdButton, savedButton)
+        createdContainer.classList.add('image-grid')
+        resizeAllGridItems()
+    });
+    savedButton.addEventListener('click', () => {
+        toggleActive(savedButton, createdButton)
+        createdContainer.classList.remove('image-grid')
+    });
 
     const editBoardButton = document.getElementById('edit-board-btn');
 
