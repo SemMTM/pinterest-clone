5cbc1a7550a574600260211938f499d2327f7b48
Osama Hendawi <osama.hendawi@hotmail.com>
2025-01-24 16:07:32 +0000

    add html templates for login pages

--- pinterest_clone/settings.py
+++ pinterest_clone/settings.py
@@ -153,19 +153,3 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django.template': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-        },
-    },
-}
