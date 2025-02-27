b914740432e9cd92470d3a5e9457b12cc1d16c91
SemMTM <osama.hendawi@hotmail.com>
2025-02-27 10:07:46 +0000

    FEAT: add 403, 404 and 500 error pages

--- pinterest_clone/urls.py
+++ pinterest_clone/urls.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
+
+
+# Custom error handlers
+def custom_403_view(request, exception):
+    return render(request, "errors/403.html", status=403)
+
+
+def custom_404_view(request, exception):
+    return render(request, "errors/404.html", status=404)
+
+
+def custom_500_view(request):
+    return render(request, "errors/500.html", status=500)
+
+
+handler403 = custom_403_view
+handler404 = custom_404_view
+handler500 = custom_500_view
 
 
 urlpatterns = [
